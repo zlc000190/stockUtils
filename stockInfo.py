9dd3d9cd109c5d402858931c55675125a1a8ea89
北京-蔡月 <caiyue@jd.com>
2017-08-19 17:31:06 +0800

    init

---
+++ stockInfo.py
@@ -0,0 +1,223 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#author:蔡董
+#date:2017.8.16
+
+import  os
+import  sys
+import  urllib
+import  re
+import simplejson
+import json
+import os.path as fpath
+
+#数据库，股票code为主键，保存，股票开盘价格，闭市价格，最高，最低，涨幅，所属于的概念，助理流入资金，需要3天的这种数据
+#行业涨跌情况最近5天的数据
+#股票和行业对应起来
+
+#东方财富网-今日最高
+todayMaxPriceUrl = 'http://xuanguapi.eastmoney.com/Stock/JS.aspx?type=xgq&sty=xgq&token=eastmoney&c=[hqzb03]&p=1&jn=moJuuzHq&ps=100&s=hqzb03&st=-1&r=1503071461051'
+#3日新高
+threeDaysMaxPriceUrl = 'http://xuanguapi.eastmoney.com/Stock/JS.aspx?type=xgq&sty=xgq&token=eastmoney&c=[hqzb03][hqzb06(1|3)]&p=1&jn=ScQYjbVF&ps=100&s=hqzb06(1|3)&st=-1&r=1503071688010'
+
+#连续涨3天以上
+moreAndMoreMaxPriceUrl = 'http://xuanguapi.eastmoney.com/Stock/JS.aspx?type=xgq&sty=xgq&token=eastmoney&c=[hqzb07(4|3)]&p=1&jn=xiZaGiTW&ps=300&s=hqzb07(4|3)&st=-1'
+
+#行业研报
+hangyeReportUrl = 'http://datainterface.eastmoney.com//EM_DataCenter/js.aspx?type=SR&sty=HYSR&mkt=0&stat=0&cmd=4&code=&sc=&ps=100&p=2&js=var%20WPaQnqfN={%22data%22:[(x)],%22pages%22:%22(pc)%22,%22update%22:%22(ud)%22,%22count%22:%22(count)%22}&rt=50102402'
+
+#资金流入排行
+zjlrRank = 'http://nufm.dfcfw.com/EM_Finance2014NumericApplication/JS.aspx/JS.aspx?type=ct&st=(BalFlowMain)&sr=-1&p=2&ps=50&js=var%20ZGdnEqhJ={pages:(pc),date:%222014-10-22%22,data:[(x)]}&token=894050c76af8597a853f5b408b759f5d&cmd=C._AB&sty=DCFFITA&rt=50102411'
+
+#东方财富网-公司被调研次数
+dytjBaseUrl = 'http://data.eastmoney.com/DataCenter_V3/jgdy/gsjsdy.ashx?pagesize=100&page=1&js=var%20MavaIUBM&param=&sortRule=-1&sortType=2'
+
+#东方财富网-券商推荐公司
+tjgsBaseUrl = 'http://datainterface.eastmoney.com//EM_DataCenter/js.aspx?type=SR&sty=GGSR&js=var%20jXudgyZA={%22data%22:[(x)],%22pages%22:%22(pc)%22,%22update%22:%22(ud)%22,%22count%22:%22(count)%22}&ps=100&p=2&mkt=0&stat=0&cmd=2&code='
+
+#东方财富网-股东增持
+gdzcBaseUrl = 'http://data.eastmoney.com/DataCenter_V3/gdzjc.ashx?pagesize=50&page=2&js=var%20ukjJZiRW&param=&sortRule=-1&sortType=BDJZ&tabid=jzc&code=&name=&rt=50102353'
+
+#概念涨幅排行
+gnzfBaseUrl = 'http://nufm.dfcfw.com/EM_Finance2014NumericApplication/JS.aspx?cmd=C._BKGN&type=ct&st=(ChangePercent)&sr=-1&p=1&ps=50&js=var%20vXdHFFJl={pages:(pc),data:[(x)]}&token=894050c76af8597a853f5b408b759f5d&sty=DCFFITABK&rt=50102372'
+
+
+def getHtmlFromUrl(url):
+    # setCookie2()
+    # req = urllib2.Request(url,None,header)
+    res = urllib.urlopen(url).read()
+    # return res.read()
+
+    return res.decode('gbk', 'ignore').encode('utf-8')
+
+
+def getJsonObj(obj):
+    #"var moJuuzHq="{"Results":["2,300672,国科微,是","2,300676,华大基因,是","1,603612,索通发展,是","1,603707,健友股份,是","2,002888,惠威科技,是","2,300678,中科信息,是","2,002889,东方嘉盛,是","1,603860,中公高科,是","2,300685,艾德生物,是","2,300687,赛意信息,是","1,603880,南卫股份,是","2,300689,澄天伟业,是","1,603602,纵横通信,是","2,300688,创业黑马,是","1,603721,中广天择,是","2,300691,联合光电,是","1,601326,秦港股份,是","1,603776,永安行,是","2,002892,科力尔,是","1,603129,春风动力,是","1,603557,起步股份,是"],"AllCount":"21","PageCount":"1","AtPage":"1","PageSize":"40","ErrMsg":"","UpdateTime":"2017/8/19 13:37:03","TimeOut":"3ms"}"
+    newobj = obj.split('=')[1]  #//必须要将前面的= 去掉
+    return  simplejson.loads(newobj)
+
+def getJsonObj2(obj):
+     newobj = "{" + obj.split('={')[1]
+     return simplejson.loads(newobj)
+
+class CompanyInfo(object):
+    def __init__(self,code,name):
+        super(CompanyInfo,self).__init__()
+        self.code = code
+        self.name = name
+
+class CompanyResearchReport(CompanyInfo):
+    def __init__(self,code,name ,startTime,desc,sum):
+        super(CompanyResearchReport,self).__init__(code,name)
+        self.time = startTime
+        self.desc  = desc
+        self.sum = sum
+
+
+class StockEachDayInfo(CompanyInfo):
+    '''每一天的数据行情'''
+    def __init__(self,code,name,startPrice,endPrice,maxPrice,minPrice,inflowCount,concept):
+        super(StockEachDayInfo,self).__init__(code,name)
+        self.startPrice = startPrice
+        self.endPrice = endPrice
+        self.maxPrice = maxPrice
+        self.minPrice = minPrice
+        self.inflowCount = inflowCount
+        self.concept = concept
+
+
+class StockLatestInfo(CompanyInfo):
+    '''股票最近数据行情,保存的是 StockEachDayInfo 的数据'''
+    def __init__(self,eachDayInfo):
+        super(StockLatestInfo,self)
+        self.stockInfo = []
+        self.stockInfo.append(eachDayInfo)
+
+    def addStockDayInfo(self,dayInfo):
+        self.stockInfo.append(dayInfo)
+
+
+
+class StockUtils(object):
+    def __init__(self):
+        super(StockUtils,self)
+
+    @classmethod
+    def getTodayMaxStockList(self):
+        '''当日创新高'''
+        res =  getHtmlFromUrl(todayMaxPriceUrl)
+        companyListObj = getJsonObj(res)
+        if companyListObj:
+            list =  companyListObj['Results']
+            cList = []
+            if list and len(list):
+                for item in list:
+                    '''item 是字符串，应该分割处理'''
+                    stockInfo = item.split(',')
+                    cinfo = CompanyInfo(stockInfo[1],stockInfo[2])
+                    cList.append(cinfo)
+                return cList
+
+        return  None
+
+    @classmethod
+    def getThreeDaysMaxStockList(self):
+        '''最近三天创新高'''
+        res = getHtmlFromUrl(threeDaysMaxPriceUrl)
+        companyListObj = getJsonObj(res)
+        if companyListObj:
+            list =  companyListObj['Results']
+            cList = []
+            if list and len(list):
+                for item in list:
+                    '''item 是字符串，应该分割处理'''
+                    stockInfo = item.split(',')
+                    cinfo = CompanyInfo(stockInfo[1],stockInfo[2])
+                    cList.append(cinfo)
+                return cList
+
+        return  None
+
+    @classmethod
+    def getIndustryReport(self):
+        '''行业调研'''
+        res = getHtmlFromUrl(hangyeReportUrl)
+        companyListObj = getJsonObj(res)
+        if companyListObj:
+            list = companyListObj['data']
+            cList = []
+            if list and len(list):
+                for item in list:
+                    cList.append(item)
+                return cList
+        return None
+
+    @classmethod
+    def getCompanyResearchRank(self):
+        '''公司被调研次数排行'''
+        res = getHtmlFromUrl(dytjBaseUrl)
+        companyListObj = getJsonObj2(res)
+        if companyListObj:
+            list = companyListObj['data']
+            cList = []
+            if list and len(list):
+                for item in list:
+                    cinfo = CompanyResearchReport(item['CompanyCode'],item['CompanyName'], item['StartDate'],item['Description'],item['OrgCode'])
+                    cList.append(cinfo)
+                return cList
+        else:
+            return None
+
+
+    @classmethod
+    def getRcommandedCompanyList(self):
+        '''券商推荐'''
+        res = getHtmlFromUrl(tjgsBaseUrl)
+        companyListObj = getJsonObj(res)
+        if companyListObj:
+            list = companyListObj['data']
+            cList = []
+            if list and len(list):
+                for item in list:
+                    '''item 是字符串，应该分割处理'''
+                    stockInfo = item.split(',')
+                    cinfo = CompanyInfo(stockInfo[1], stockInfo[2])
+                    cList.append(cinfo)
+                return cList
+
+        return None
+
+
+    def getStockholderHoldsStocks(self):
+        '''股东增持'''
+
+        pass
+
+    def getIndustryRank(self):
+        '''行业涨幅'''
+        pass
+
+if __name__ == '__main__':
+    util = StockUtils()
+    #当天创新高
+    li =  util.getTodayMaxStockList()
+    # for item in li:
+    print li 
+
+    #最近3天创新高
+    #print util.getThreeDaysMaxStockList()
+
+    #行业报告
+    #print util.getIndustryReport()
+
+
+    #调研次数
+    #print util.getCompanyResearchRank()
+
+    #推荐公司
+    #print util.getRcommandedCompanyList()
+
+
+
+
+
