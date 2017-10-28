ee001484a8d8627499b86f4f01578a0db8709bdf
caiyue <1072079578@qq.com>
2017-10-28 19:27:11 +0800

    asdfasdf

--- mysqlOperation.py
+++ mysqlOperation.py
@@ -14,36 +14,37 @@ conn = MySQLdb.connect(host='localhost',
                        )
 
 class mysqlOp(object):
-    def __init__(self):
-        super(mysqlOp,self).__init__()
-        self.conn = conn
-        self.cur = conn.cursor()
-
-    def executeSQL(self,sql):
-        '''初始化数据'''
-        if sql and len(sql) > 0:
-            #return self.cur.execute(sql)
-            return None
-        else:
-            return  None
-    def getNewestDate(self):
-        '''获取最新股票信息日期'''
-        self.cur.execute('select  distinct sdate from  stock5DayDetailData')
-        string =  self.cur.fetchall()[0][0]
-        return string.split(' ')[0]
-
-    def moveDataIntables(self,tablelist):
-        '''必须要至少2个元素，现在是写死的10个'''
-        if len(tablelist) < 2:return
-        for table in tablelist:
-            if table == tablelist[0]:
-                self.clearTableData(table)
-            else:
-                index = tablelist.index(table) - 1
-                src = table
-                dst = tablelist[index]
-                self.clearTableData(dst)
-                sql = 'insert into %s(code,sname,startPrice,minPrice,maxPrice,endPrice,inflowCount,sdate,priceIncrementPercent) select code,sname,startPrice,minPrice,maxPrice,endPrice,inflowCount,sdate,priceIncrementPercent from %s' % (dst,src)
-                self.executeSQL(sql)
-    def clearTableData(self,tableName):
-        self.executeSQL('delete from %s' % tableName)
+    pass
+    # def __init__(self):
+    #     super(mysqlOp,self).__init__()
+    #     self.conn = conn
+    #     self.cur = conn.cursor()
+    #
+    # def executeSQL(self,sql):
+    #     '''初始化数据'''
+    #     if sql and len(sql) > 0:
+    #         #return self.cur.execute(sql)
+    #         return None
+    #     else:
+    #         return  None
+    # def getNewestDate(self):
+    #     '''获取最新股票信息日期'''
+    #     self.cur.execute('select  distinct sdate from  stock5DayDetailData')
+    #     string =  self.cur.fetchall()[0][0]
+    #     return string.split(' ')[0]
+    #
+    # def moveDataIntables(self,tablelist):
+    #     '''必须要至少2个元素，现在是写死的10个'''
+    #     if len(tablelist) < 2:return
+    #     for table in tablelist:
+    #         if table == tablelist[0]:
+    #             self.clearTableData(table)
+    #         else:
+    #             index = tablelist.index(table) - 1
+    #             src = table
+    #             dst = tablelist[index]
+    #             self.clearTableData(dst)
+    #             sql = 'insert into %s(code,sname,startPrice,minPrice,maxPrice,endPrice,inflowCount,sdate,priceIncrementPercent) select code,sname,startPrice,minPrice,maxPrice,endPrice,inflowCount,sdate,priceIncrementPercent from %s' % (dst,src)
+    #             self.executeSQL(sql)
+    # def clearTableData(self,tableName):
+    #     self.executeSQL('delete from %s' % tableName)
